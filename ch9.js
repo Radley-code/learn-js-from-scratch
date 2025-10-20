973ce0e471d3a5f0063246ce6613c9850fdcb9d6
Radley-code <radleyacha00@icloud.com>
2025-10-20 23:12:50 +0100

    Exercises and project chapter

--- ch9.js
+++ ch9.js
@@ -2,3 +2,12 @@ const eles = document.querySelectorAll(".output");
 console.log(eles); //return a static NodeList, which represents a list of the document's element that match the selectors
 
 //Practice Exercise 9.3
+const output = document.querySelector('.output');
+output.textContent = "Hello World";
+output.classList.add("red");
+output.id = "tester";
+output.style.backgroundColor ="red";
+console.log(document.URL);
+output.textContent = document.URL;
+
+
