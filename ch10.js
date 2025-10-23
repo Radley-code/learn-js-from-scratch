3a84bb55f00f637cd0804916c823c723c29bf6a3
Radley-code <radleyacha00@icloud.com>
2025-10-23 20:06:47 +0100

    Add chapter 10

---
+++ ch10.js
@@ -0,0 +1,52 @@
+// In this chapter, we will do basic DOM traversing using the document object that we saw in the previous chapter
+// we can do this by stepping into the document object and navigating our way from there onwards
+
+// we'll be using the broswer's console, enter these code onto the browser's console
+
+// Practice Exercise
+//more practices on traversing element
+
+document.body.children.greeting.innerText = "bye!";
+
+//but we use this to return as plain text. if replace bye! in line 9 with <b>Bye!</b>
+//it displays <b>Bye!</b>
+//With HTML around it, we use innerHTML instead of innerText.
+document.body.children.greeting.innerHTML = "<b>Bye!</b>"; //returns the Bye! in bold
+//selecting element as objects
+
+//Accessing elements in the DOM
+// using the console in the browser
+document.getElementById("two");
+// returns the HTML element
+
+//Practice exercise 10.2
+
+//Accessing elements by tag name
+document.getElementsByTagName("div");
+// we can access them using the item() method to access them by index
+document.getElementsByTagName("div").item(1);
+
+document.getElementsByTagName("div").namedItem("one"); // we can also access them by name, using namedItem() method.
+
+//Practice exercise 10.3
+// create html elements
+const myEles = document.getElementsByTagName("div");
+console.log(myEles[1]);
+
+//Accessing elements by class name
+document.getElementsByClassName("example"); //returns the div tags with the example class.
+//Practice exercise 10.4
+
+//Accessing elements elements with a CSS selector
+//we can also access using the a css selector. querySelector() and querySelectionAll().
+document.querySelector("p"); //select the first element that matches the query
+
+document.querySelectorAll("p"); // returns all elements that match the query
+//Example
+document.querySelectorAll("div"); // returns all divs in the html file.
+
+//Element Click handler.
+function stop() {
+  alert("Ouch! Stop it!");
+} //defining the alert function which triggers wen the div is clicked
+
--- ch9.js
+++ ch9.js
@@ -1,5 +1,5 @@
-const eles = document.querySelectorAll(".output");
-console.log(eles); //return a static NodeList, which represents a list of the document's element that match the selectors
+const eles1 = document.querySelectorAll(".output");
+// console.log(eles); //return a static NodeList, which represents a list of the document's element that match the selectors
 
 //Practice Exercise 9.3
 const output = document.querySelector('.output');
@@ -9,5 +9,22 @@ output.id = "tester";
 output.style.backgroundColor ="red";
 console.log(document.URL);
 output.textContent = document.URL;
+
+//Chapter Project 
+//Already selected element with the class output
+    const mainList = output.querySelector("ul");
+    mainList.id = "mainList";
+    console.log(mainList);
+    const eles = document.querySelectorAll("div");
+    for (let x = 0; x < eles.length; x++) {
+        console.log(eles[x].tagName);
+        eles[x].id = "id" + (x+1);
+        if (x % 2) {
+            eles[x].style.color = "red";
+        } else {
+             eles[x].style.color = "blue";
+            
+        }
+    } //needs attention.
 
 
